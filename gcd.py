609eeacb70c62d32c29c87ba773ecad69874a56f
Peter Ezzat <pete_ezzat@hotmail.com>
2018-02-16 12:22:28 +0200

    Make sure that inputs must be more than 1 input.

--- gcd.py
+++ gcd.py
@@ -1,12 +1,19 @@
 def get_gcd(numbers):
 	
-
+	pass
+	
 #.#.#.
 
 numbers = input("Numbers: ")
+
+while len(numbers) < 2:
+	print("\nNumbers must be more than 1 input.")
+	numbers = input("Numbers: ")
 
 # Convert Strings to Ints
 numbers = numbers.split(" ")
 numbers = list(map(int, numbers))
 
-gcd = get_gcd(numbers)
+print(numbers)
+
+#gcd = get_gcd(numbers)
